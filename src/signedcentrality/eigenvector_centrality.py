cbf12eb9c99056ef76de42e84e9c8e2ed8d740c7
VirgileSucal <virgile.sucal@alumni.univ-avignon.fr>
2020-06-10 21:56:57 +0200

    compute_eigenvector_centrality() works correctly
    compute_eigenvector_centrality() now returns values with the right sign.
    
    Remove parameter D in compute_eigenvector_centrality().
    Remove useless function diagonal().
    Remove useless test method test_diagonal().
    
    Updated files:
        eigenvector_centrality.py
        tests.py

--- src/signedcentrality/eigenvector_centrality.py
+++ src/signedcentrality/eigenvector_centrality.py
@@ -2,9 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from sys import float_info
-from numpy import real, array
+from numpy import real
 from numpy.linalg import norm
-from scipy.sparse import lil_matrix
 from scipy.sparse.linalg import eigs
 from signedcentrality._utils.utils import *
 
@@ -17,42 +16,16 @@ The measure is computed by following the method of Phillip Bonacich and Paulette
 """
 
 
-def diagonal(n1, n2):
-	"""
-	Create a diagonal squared matrix.
-
-	Values are set as explained in the article written by P. Bonacich and P. Lloyd.
-	Using scipy.sparse.lil_matrix is more efficient to contruct the matrix.
-	Since it is to slow to compute arithmetic operations, it is converted in csr_matrix which is more efficient to such operations.
-
-	:param n1: number of individuals in the first set
-	:type n1: int
-	:param n2: number of individuals in the second set
-	:type n2: int
-	:return: the matrix
-	:rtype: scipy.sparse.csr_matrix
-	"""
-
-	diag = lil_matrix(array([array([0. for _1 in range(n1 + n2)]) for _0 in range(n1 + n2)]))  # Create a squared matrix of floats which is initialized with the float value -1.
-	diag.setdiag(-1.)
-	diag.setdiag([1. for _ in range(n1)])
-	return diag.tocsr()
-
-
-def compute_eigenvector_centrality(graph, D=None, scaled=False):
+def compute_eigenvector_centrality(graph, scaled=False):
 	"""
 	Compute the eigenvector centrality.
 
-	If the graph if signed, setting the matrix D enables this function to return a centrality with the right sign.
-	If this argument isn't set, the magnitude will be right, but the signs may be inverted.
-
-	Thus, if D is set, the function solves A * x = λ * D * x instead of A * x = λ * x.
-	D have to be a signed diagonal matrix.
-	Values have to be set as explained in the article written by P. Bonacich and P. Lloyd.
-	The function diagonal() creates such a matrix.
+	If scaled is True, the values will be set such that the maximum is 1.
 
 	:param graph: the graph
 	:type graph: igraph.Graph
+	:param scaled: the graph
+	:type scaled: bool
 	:return: the eigenvector centrality
 	:rtype: list
 	"""
@@ -66,7 +39,7 @@ def compute_eigenvector_centrality(graph, D=None, scaled=False):
 			eigs(              # Because it is a square matrix.
 				matrix,        # The matrix.
 				1,             # To get only the first eigenvector.
-				D,             # If D is set, eigs() will solve A * x = λ * D * x.
+				None,          # Default value.
 				None,          # Default value.
 				"LR"           # Because the matrix is treated as a complex matrix.
 				)[1]           # To get only the eigenvector (the eigenvalue is not used).
@@ -74,13 +47,16 @@ def compute_eigenvector_centrality(graph, D=None, scaled=False):
 
 	centrality = [value * (1 / norm(eigenvector)) for value in eigenvector]  # If the norm isn't 1, it makes the result more accurate.
 
-	if scaled:
+	if scaled:  # Sets the values such that the maximum is 1
 		max_value = float_info.min  # Minimal value of a float
 		for value in centrality:
 			if abs(value) > max_value:  # abs(), because the magnitude must be scaled, not the signed value.
 				max_value = abs(value)
 		scale = 1 / max_value
 	# else, the scale remains 1.
+
+	if sum(centrality) < 0:  # Makes the first cluster values positive if they aren't.
+		scale *= -1  # Values will be inverted when they will be scaled (more efficient).
 
 	scaled_centrality = [value * scale for value in centrality]
 
--- tests/tests.py
+++ tests/tests.py
@@ -8,8 +8,7 @@ from scipy.sparse import csr_matrix
 from signedcentrality.eigenvector_centrality import *
 # noinspection PyProtectedMember
 from signedcentrality._utils.utils import *
-from numpy import trunc, ndarray
-import numpy
+from numpy import trunc, ndarray, array
 
 """
 This module contains unit tests for the modules of the package signedcentrality.
@@ -43,75 +42,75 @@ class SignedCentralityTest(unittest.TestCase):
 
 	def test_get_matrix_network_a(self):
 		matrix = self.matrix['a']
-		array = self.array['a']
+		array_a = self.array['a']
 
 		self.assertIsInstance(matrix, csr_matrix)
-		self.assertIsInstance(array, ndarray)
+		self.assertIsInstance(array_a, ndarray)
 
-		array_test = numpy.array(
+		array_test = array(
 			[
-				numpy.array([0, 0, 1, 0, 0]),
-				numpy.array([0, 0, 1, 0, 0]),
-				numpy.array([1, 1, 0, 1, 1]),
-				numpy.array([0, 0, 1, 0, 1]),
-				numpy.array([0, 0, 1, 1, 0])
+				array([0, 0, 1, 0, 0]),
+				array([0, 0, 1, 0, 0]),
+				array([1, 1, 0, 1, 1]),
+				array([0, 0, 1, 0, 1]),
+				array([0, 0, 1, 1, 0])
 				])  # Undirected graph.
 
-		for i in range(len(array)):
-			for j in range(len(array[i])):
-				self.assertEqual(array[i][j], array_test[i][j])
+		for i in range(len(array_a)):
+			for j in range(len(array_a[i])):
+				self.assertEqual(array_a[i][j], array_test[i][j])
 
 	def test_get_matrix_network_b(self):
 		matrix = self.matrix['b']
-		array = self.array['b']
+		array_b = self.array['b']
 
 		self.assertIsInstance(matrix, csr_matrix)
-		self.assertIsInstance(array, ndarray)
+		self.assertIsInstance(array_b, ndarray)
 
-		array_test = numpy.array(
+		array_test = array(
 			[
-				numpy.array([0, 0, 1, 0, 0]),
-				numpy.array([0, 0, 1, 0, 0]),
-				numpy.array([1, 1, 0, 1, -1]),
-				numpy.array([0, 0, 1, 0, 1]),
-				numpy.array([0, 0, -1, 1, 0])
+				array([0, 0, 1, 0, 0]),
+				array([0, 0, 1, 0, 0]),
+				array([1, 1, 0, 1, -1]),
+				array([0, 0, 1, 0, 1]),
+				array([0, 0, -1, 1, 0])
 				])  # Undirected graph.
 
-		for i in range(len(array)):
-			for j in range(len(array[i])):
-				self.assertEqual(array[i][j], array_test[i][j])
+		for i in range(len(array_b)):
+			for j in range(len(array_b[i])):
+				self.assertEqual(array_b[i][j], array_test[i][j])
 
 	def test_get_matrix_sampson(self):
 		matrix = self.matrix['s']
-		array = self.array['s']
+		array_s = self.array['s']
 
 		self.assertIsInstance(matrix, csr_matrix)
-		self.assertIsInstance(array, ndarray)
+		self.assertIsInstance(array_s, ndarray)
 
-		array_test = numpy.array([
-				numpy.array([0, 1, 1, 0, 0, 0, -1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0]),
-				numpy.array([1, 0, 0, -1, 0, 0, 1, -1, 0, 0, -1, 0, -1, 0, 1, 1, 0, 0]),
-				numpy.array([1, 0, 0, -1, 0, -1, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-				numpy.array([0, -1, -1, 0, 1, 1, -1, 1, 0, 1, 0, 0, 0, -1, 0, -1, 0, 0]),
-				numpy.array([0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-				numpy.array([0, 0, -1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0]),
-				numpy.array([-1, 1, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-				numpy.array([0, -1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, -1]),
-				numpy.array([0, 0, -1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-				numpy.array([0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-				numpy.array([0, -1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-				numpy.array([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-				numpy.array([0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 1]),
-				numpy.array([1, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-				numpy.array([0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0]),
-				numpy.array([0, 1, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-				numpy.array([0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]),
-				numpy.array([0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0])
+		array_test = array([
+				array([0, 1, 1, 0, 0, 0, -1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0]),
+				array([1, 0, 0, -1, 0, 0, 1, -1, 0, 0, -1, 0, -1, 0, 1, 1, 0, 0]),
+				array([1, 0, 0, -1, 0, -1, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+				array([0, -1, -1, 0, 1, 1, -1, 1, 0, 1, 0, 0, 0, -1, 0, -1, 0, 0]),
+				array([0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+				array([0, 0, -1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0]),
+				array([-1, 1, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+				array([0, -1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, -1]),
+				array([0, 0, -1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+				array([0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+				array([0, -1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+				array([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+				array([0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 1]),
+				array([1, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+				array([0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0]),
+				array([0, 1, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+				array([0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]),
+				array([0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0])
 			])  # Undirected graph, created from a directed graph.
 
-		for i in range(len(array)):
-			for j in range(len(array[i])):
-				self.assertEqual(array[i][j], array_test[i][j])
+		for i in range(len(array_s)):
+			for j in range(len(array_s[i])):
+				self.assertEqual(array_s[i][j], array_test[i][j])
 
 	def test_write_graph(self):
 		write_graph(self.graph['s'], self._test_path_name)
@@ -123,46 +122,17 @@ class SignedCentralityTest(unittest.TestCase):
 			for j in range(len(self.array['s'][i])):
 				self.assertEqual(self.array['s'][i][j], array_test[i][j])
 
-	def test_diagonal(self):
-		n1 = 4
-		n2 = 4
-		length = n1 + n2
-		diag = diagonal(n1, n2).toarray()
-
-		array_test = numpy.array(
-			[
-				numpy.array([1, 0, 0, 0, 0, 0, 0, 0]),
-				numpy.array([0, 1, 0, 0, 0, 0, 0, 0]),
-				numpy.array([0, 0, 1, 0, 0, 0, 0, 0]),
-				numpy.array([0, 0, 0, 1, 0, 0, 0, 0]),
-				numpy.array([0, 0, 0, 0, -1, 0, 0, 0]),
-				numpy.array([0, 0, 0, 0, 0, -1, 0, 0]),
-				numpy.array([0, 0, 0, 0, 0, 0, -1, 0]),
-				numpy.array([0, 0, 0, 0, 0, 0, 0, -1]),
-				])
-
-		for i in range(length):
-			for j in range(length):
-				self.assertEqual(diag[i][j], array_test[i][j])
-
 	def test_compute_eigenvector_centrality_network_a(self):
-		self.assertSequenceEqual([round(i, 2) for i in compute_eigenvector_centrality(self.graph['a'], None, True)], [.43, .43, 1., .74, .74])
+		self.assertSequenceEqual([round(i, 2) for i in compute_eigenvector_centrality(self.graph['a'], True)], [.43, .43, 1., .74, .74])
 
 	def test_compute_eigenvector_centrality_network_b(self):
 		# self.assertSequenceEqual([round(i, 2) for i in compute_eigenvector_centrality(self.graph['b'], None, True)], [.55, .55, 1., .35, -.35])  #
-		# self.assertSequenceEqual([trunc(i * 100) / 100 for i in compute_eigenvector_centrality(self.graph['b'], None, True)], [.55, .55, 1., .35, -.35])  # There aren't any problems if the result is truncated.
-		self.assertSequenceEqual([abs(trunc(i * 100) / 100) for i in compute_eigenvector_centrality(self.graph['b'], None, True)], [abs(i) for i in [.55, .55, 1., .35, -.35]])  # There aren't any problems if the result is truncated.
+		self.assertSequenceEqual([trunc(i * 100) / 100 for i in compute_eigenvector_centrality(self.graph['b'], True)], [.55, .55, 1., .35, -.35])  # There aren't any problems if the result is truncated.
 
 	def test_compute_eigenvector_centrality_sampson(self):
 		result = [round(i, 3) for i in compute_eigenvector_centrality(self.graph['s'])]
-		# test = [.174, .188, .248, .319, .420, .219, .365, -.081, -.142, -.292, -.088, -.123, -.217, -.072, -.030, -.254, -.282, -.287]
-		# result.sort(reverse=True)
-		# test.sort(reverse=True)
 		test = [round(value, 3) for _, value in OrderedDict(sorted(eigenvector_centrality(from_scipy_sparse_matrix(get_matrix(self.graph['s']), False, None, WEIGHT), weight=WEIGHT).items())).items()]
-		print(result)
-		print(test)
-		# self.assertSequenceEqual(result, test)
-		self.assertSequenceEqual([abs(i) for i in result], [abs(i) for i in test])
+		self.assertSequenceEqual(result, test)
 
 
 if __name__ == '__main__':
