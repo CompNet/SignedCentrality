ed59ba2508fc3bdbacd7196c14b98110097dbf1d
VirgileSucal <virgile.sucal@alumni.univ-avignon.fr>
2021-01-07 10:19:23 +0100

    Add SNE initial code
    Add packages node_embeddings.sne and node_embeddings.sne.sne.
    Add use instructions.
    Add unaltered latest version for SNE code.
    Add files which are created by running the code in .gitignore.
    
    Updated files:
    	.gitignore
    	node_embeddings/__init__.py
    
    Added files:
    	node_embeddings/sne/__init__.py
    	node_embeddings/sne/sne/__init__.py
    	README.md
    	SNE.py
    	test.py
    	walk.py
    	wiki_edit.txt
    	wiki_usr_labels.txt

---
+++ src/node_embeddings/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+This package contains modules related to node embeddings.
+
+Each module of the package contains classes that compute a node embedding.
+
+@author: Virgile Sucal
+
+.. warning: In order to use this package, one has to install igraph and scipy libraries.
+
+.. seealso: igraph
+.. seealso: scipy
+"""
+
---
+++ src/node_embeddings/sne/sne/walk.py
@@ -0,0 +1,209 @@
+# -*- coding: utf-8 -*-
+
+"""Graph utilities."""
+
+import logging
+import sys
+from io import open
+from os import path
+from time import time
+from glob import glob
+from six.moves import range, zip, zip_longest
+from six import iterkeys
+from collections import defaultdict, Iterable
+import random
+
+
+logger = logging.getLogger("deepwalk")
+
+__author__ = "Bryan Perozzi"
+__email__ = "bperozzi@cs.stonybrook.edu"
+
+LOGFORMAT = "%(asctime).19s %(levelname)s %(filename)s: %(lineno)s %(message)s"
+
+
+class Graph(defaultdict):
+    """Efficient basic implementation of nx `Graph' â€“ Undirected graphs with self loops"""
+
+    def __init__(self):
+        super(Graph, self).__init__(list)
+
+    def nodes(self):
+        return self.keys()
+
+    def adjacency_iter(self):
+        return self.iteritems()
+
+    def subgraph(self, nodes={}):
+        subgraph = Graph()
+
+        for n in nodes:
+            if n in self:
+                subgraph[n] = [x for x in self[n] if x in nodes]
+
+        return subgraph
+
+    def make_undirected(self):
+
+        t0 = time()
+
+        for v in self.keys():
+            for other in self[v]:
+                if v != other:
+                    self[other].append(v)
+
+        t1 = time()
+        logger.info('make_directed: added missing edges {}s'.format(t1 - t0))
+
+        self.make_consistent()
+        return self
+
+    def make_consistent(self):
+        t0 = time()
+        for k in iterkeys(self):
+            self[k] = list(sorted(set(self[k])))
+
+        t1 = time()
+        logger.info('make_consistent: made consistent in {}s'.format(t1 - t0))
+
+        self.remove_self_loops()
+
+        return self
+
+    def remove_self_loops(self):
+
+        removed = 0
+        t0 = time()
+
+        for x in self:
+            if x in self[x]:
+                self[x].remove(x)
+                removed += 1
+
+        t1 = time()
+
+        logger.info('remove_self_loops: removed {} loops in {}s'.format(removed, (t1 - t0)))
+        return self
+
+    def check_self_loops(self):
+        for x in self:
+            for y in self[x]:
+                if x == y:
+                    return True
+
+        return False
+
+    def has_edge(self, v1, v2):
+        if v2 in self[v1] or v1 in self[v2]:
+            return True
+        return False
+
+    def degree(self, nodes=None):
+        if isinstance(nodes, Iterable):
+            return {v: len(self[v]) for v in nodes}
+        else:
+            return len(self[nodes])
+
+    def order(self):
+        "Returns the number of nodes in the graph"
+        return len(self)
+
+    def number_of_edges(self):
+        "Returns the number of nodes in the graph"
+        return sum([self.degree(x) for x in self.keys()]) / 2
+
+    def number_of_nodes(self):
+        "Returns the number of nodes in the graph"
+        return order()
+
+    def random_walk(self, path_length, alpha=0, rand=random.Random(), start=None):
+        """ Returns a truncated random walk.
+            path_length: Length of the random walk.
+            alpha: probability of restarts.
+            start: the start node of the random walk.
+        """
+        G = self
+        if start:
+            path = [start]
+        else:
+            # Sampling is uniform w.r.t V, and not w.r.t E
+            path = [rand.choice(G.keys())]
+
+        while len(path) < path_length:
+            cur = path[-1]
+            if len(G[cur]) > 0:
+                if rand.random() >= alpha:
+                    path.append(rand.choice(G[cur]))
+                else:
+                    path.append(path[0])
+            else:
+                break
+        return path
+
+
+# TODO add build_walks in here
+
+def build_deepwalk_corpus(G, num_paths, path_length, alpha=0,
+                          rand=random.Random(0)):
+    walks = []
+
+    nodes = list(G.nodes())
+
+    for cnt in range(num_paths):
+        rand.shuffle(nodes)
+        for node in nodes:
+            walks.append(G.random_walk(path_length, rand=rand, alpha=alpha, start=node))
+
+    return walks
+
+
+def build_deepwalk_corpus_iter(G, num_paths, path_length, alpha=0,
+                               rand=random.Random(0)):
+    nodes = list(G.nodes())
+
+    for cnt in range(num_paths):
+        rand.shuffle(nodes)
+        for node in nodes:
+            yield G.random_walk(path_length, rand=rand, alpha=alpha, start=node)
+
+
+def grouper(n, iterable, padvalue=None):
+    "grouper(3, 'abcdefg', 'x') --> ('a','b','c'), ('d','e','f'), ('g','x','x')"
+    return zip_longest(*[iter(iterable)] * n, fillvalue=padvalue)
+
+
+def load_edgelist(file_, undirected=True):
+    G = Graph()
+    with open(file_) as f:
+        for l in f:
+            x, y = l.strip().split()[:2]
+            G[x].append(y)
+            if undirected:
+                G[y].append(x)
+    G.make_consistent()
+    return G
+
+
+def count_lines(f):
+    if path.isfile(f):
+        num_lines = sum(1 for line in open(f))
+        return num_lines
+    else:
+        return 0
+
+def write_walks_to_disk(G, f, num_paths, path_length, alpha, rand):
+    # G, f, num_paths, path_length, alpha, rand = args
+    t_0 = time()
+    with open(f, 'w') as fout:
+        for walk in build_deepwalk_corpus_iter(G=G, num_paths=num_paths, path_length=path_length,
+                                                     alpha=alpha, rand=rand):
+          fout.write(u"{}\n".format(u" ".join(str(v) for v in walk)))
+    logger.debug("Generated new file {}, it took {} seconds".format(f, time() - t_0))
+    return f
+
+
+
+number_walks = 20
+walk_length = 40
+G = load_edgelist('data/wiki_edit.txt')
+f = write_walks_to_disk(G, f='data/wiki_edit_num_40.walk', num_paths=number_walks, path_length=walk_length, alpha=0, rand=random.Random(1024))
