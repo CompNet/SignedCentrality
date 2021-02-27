32cb062c959d353bb73366681d6c89d8dd522f0c
FaureA-CERI <56632304+FaureA-CERI@users.noreply.github.com>
2021-02-27 12:12:58 +0100

    Update RWR_centrality.py

--- src/centrality/RWR_centrality.py
+++ src/centrality/RWR_centrality.py
@@ -1,16 +1,31 @@
 from pyrwr.rwr import RWR
 
 
-def rwr(g, graph_type):
+def perform_rwr(graph, graph_type):
+    '''
+    This method performs the srwr algorithm to calculate the centrality values for
+    the nodes of a given graph.
+
+    :param graph: i-graph object
+    :type graph: i-graph object
+    :param graph_type: indicates the type of the graph (undirected or directed)
+    :type graph_type: string
+    '''
 
     rwr = RWR()
 
     f = open("RWR_temp.txt","w+")
 
-    for i in g.es:
-        f.write(i.source, "\t", i.target, "\t", g.es['weight'][i])
+    for i in graph.es:
+        f.write(i.source, "\t", i.target, "\t", graph.es['weight'][i])
 
     f.close()
     rwr.read_graph("RWR_temp.txt", graph_type)
     r = rwr.compute(seed, c, epsilon, max_iters)
-    
+
+    rwr = []
+    for value in r:
+        rwr.append(r)
+        
+    graph.vs['rwr'] = rwr
+    return graph
